e6c3dc13a80403fe7d92c0bab17055b8d5eedf06
Somesh Raj <someshraj78569@gmail.com>
2025-01-21 16:24:58 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -9,8 +9,6 @@ if ($conn->connect_error) {
     die("Database Connection Failed: " . $conn->connect_error);  
 } else {  
     echo "";  
-}  
-
-// Optionally set the character set to avoid issues with special characters  
+}   
 $conn->set_charset("utf8");  
 ?>
